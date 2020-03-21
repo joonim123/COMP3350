17244dfebd04c1e096b2ffc760715589202565c4
joonim123 <joosoh98@gmail.com>
2020-03-21 00:06:28 -0500

    updated

--- hw6/Question2.asm
+++ hw6/Question2.asm
@@ -1,8 +1,7 @@
-; //Question2
-
-TITLE Homework #6 (question2.asm)
+TITLE Homework #6
 
 INCLUDE Irvine32.inc
+INCLUDELIB Irvine32.lib
 
 .386
 .model flat, stdcall
@@ -10,33 +9,35 @@ INCLUDE Irvine32.inc
 ExitProcess PROTO dwExitCode : DWORD
 
 .data
-; //PromptUser BYTE "Please enter a value:", 0
-num BYTE "Test", 0
-numLen = ($ - num) - 1
+PromptUser BYTE "Please enter a value: ", 0
+num WORD 5 DUP (?)						; Declare 5 uninitialized values
 
 .code
 main PROC
+	mov esi, 0							; Initialize counter
 
-	mov ecx, numLen
-	mov esi, 0
+	pushLoop:
+		mov edx, OFFSET PromptUser
+		call WriteString				; Display 'PromptUser'
+		call ReadInt					; Listen for user input
+		push eax						; Feed user input into stack
+		inc esi							; Increment counter
+		cmp esi, 5						; Check if all 5 elements are initialized
+		jl pushLoop						; If there is an element still unitialized, loop 
 
-	PushLoop:
-		movzx eax, num[esi]
-		push eax
-		inc esi
-		loop PushLoop
+	mov esi, 0							; Initialize counter
 
-	mov ecx, numLen
-	mov esi, 0
-
-	PopLoop:
-		pop eax
-		mov num[esi], al
-		inc esi
-		loop PopLoop
+	popLoop:
+		pop eax							; Fetches value from the top of the stack
+		call WriteInt					; Displays the value
+		call CrLf						; Adds a new line
+		inc esi							; Increment counter
+		cmp esi, 5						; Check if all 5 elements are initialized
+		jl popLoop						; If there is an element still unitialized, loop
+		
 
 	INVOKE ExitProcess, 0
 main ENDP
 END
 
-	
+
