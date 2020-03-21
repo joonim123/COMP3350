cf3329c7ce026eea72870ec259b89a833d54632c
joonim123 <joosoh98@gmail.com>
2020-03-21 00:09:35 -0500

    fixed redundancy in sum

--- hw6/Question1.asm
+++ hw6/Question1.asm
@@ -10,15 +10,13 @@ ExitProcess PROTO dwExitCode : DWORD
 .data
 num1 WORD 3h
 num2 WORD 4h
-sum WORD ?
 
 .code
 main PROC
 mov	bx, num1
 mov	cx, num2
 add bx, cx
-mov sum, bx
-mov	ax, sum
+mov ax, bx
 
 INVOKE ExitProcess, 0
 main ENDP
